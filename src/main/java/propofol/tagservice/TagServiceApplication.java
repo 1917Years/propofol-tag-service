0fc9c30168ad7813cd19022828d5da9733e36d90
최영찬 <gks54410@gmail.com>
2022-05-03 23:47:42 +0900

    Feat: EurekaClient, ConfigurationProperties 활성화

--- src/main/java/propofol/tagservice/TagServiceApplication.java
+++ src/main/java/propofol/tagservice/TagServiceApplication.java
@@ -2,8 +2,12 @@ package propofol.tagservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
+@ConfigurationPropertiesScan(basePackages = "propofol.tagservice.api.common.properties")
 public class TagServiceApplication {
 
 	public static void main(String[] args) {
