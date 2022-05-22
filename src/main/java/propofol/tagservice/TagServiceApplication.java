5fe5bf791287509a681d9bef032ec00d9da68c5d
최영찬 <gks54410@gmail.com>
2022-05-22 21:37:57 +0900

    Feat: ModelMapper Bean 등록

--- src/main/java/propofol/tagservice/TagServiceApplication.java
+++ src/main/java/propofol/tagservice/TagServiceApplication.java
@@ -1,9 +1,11 @@
 package propofol.tagservice;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -12,6 +14,11 @@ public class TagServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TagServiceApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper createModelMapper(){
+		return new ModelMapper();
 	}
 
 }
